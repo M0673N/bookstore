8c5cb1b42f7a4b08a7cd52c689821ab90f8c5204
Stoyan Valkov <m0673n@abv.bg>
2022-04-27 17:44:02 +0300

    Laid groundwork for translation, fixed buttons and some bugs.

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('', include('bookstore.accounts.urls')),
     path('profile/', include('bookstore.profiles.urls')),
@@ -12,3 +13,6 @@ urlpatterns = [
     path('api/', include('bookstore.api.urls'))
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# django-admin makemessages -l bg
+# django-admin compilemessages
